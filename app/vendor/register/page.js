f2f1ec7c4903faf6b9acd6425d3ce5b554167e36
Akande Olalekan Toheeb <akandeolalekantoheeb94@gmail.com>
2025-01-15 02:26:01 +0100

    Vendor sign up integrated

--- app/vendor/register/page.js
+++ app/vendor/register/page.js
@@ -3,13 +3,14 @@
 import { useState } from "react";
 import Link from "next/link";
 import Header from "../../../components/Header";
+import axios from "axios";
 
 export default function VendorRegister() {
   const [formData, setFormData] = useState({
+    username: "",
     name: "",
     email: "",
     password: "",
-    confirmPassword: "",
     storeName: "",
   });
 
@@ -17,10 +18,31 @@ export default function VendorRegister() {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (e) => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(null);
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // Here you would integrate with your backend API
-    console.log("Form submitted:", formData);
+    setLoading(true);
+    // API Integration
+    const url = "/api/signup";
+    try {
+      const response = await axios.post(url, formData);
+      const data = response.data;
+
+      console.log(response.data);
+
+      if (data.status === 400) {
+        setError(data.message);
+        setLoading(false);
+        return;
+      }
+      setSuccess("Registration Successful, Proceed to login");
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -30,7 +52,26 @@ export default function VendorRegister() {
         <h1 className="text-3xl font-bold text-center text-[#19381f] mb-8">
           Vendor Registration
         </h1>
+        <p
+          className={`${error ? "text-red-500" : "text-green-500"} text-center`}
+        >
+          {error || success}
+        </p>
         <form onSubmit={handleSubmit} className="max-w-md mx-auto">
+          <div className="mb-4">
+            <label htmlFor="username" className="block mb-2">
+              Username
+            </label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+              required
+              className="w-full px-3 py-2 border rounded outline-none focus:ring-1 focus:ring-[#19381f]"
+            />
+          </div>
           <div className="mb-4">
             <label htmlFor="name" className="block mb-2">
               Name
@@ -42,7 +83,7 @@ export default function VendorRegister() {
               value={formData.name}
               onChange={handleChange}
               required
-              className="w-full px-3 py-2 border rounded"
+              className="w-full px-3 py-2 border rounded outline-none focus:ring-1 focus:ring-[#19381f]"
             />
           </div>
           <div className="mb-4">
@@ -56,7 +97,7 @@ export default function VendorRegister() {
               value={formData.email}
               onChange={handleChange}
               required
-              className="w-full px-3 py-2 border rounded"
+              className="w-full px-3 py-2 border rounded outline-none focus:ring-1 focus:ring-[#19381f]"
             />
           </div>
           <div className="mb-4">
@@ -70,23 +111,10 @@ export default function VendorRegister() {
               value={formData.password}
               onChange={handleChange}
               required
-              className="w-full px-3 py-2 border rounded"
+              className="w-full px-3 py-2 border rounded outline-none focus:ring-1 focus:ring-[#19381f]"
             />
           </div>
-          <div className="mb-4">
-            <label htmlFor="confirmPassword" className="block mb-2">
-              Confirm Password
-            </label>
-            <input
-              type="password"
-              id="confirmPassword"
-              name="confirmPassword"
-              value={formData.confirmPassword}
-              onChange={handleChange}
-              required
-              className="w-full px-3 py-2 border rounded"
-            />
-          </div>
+
           <div className="mb-4">
             <label htmlFor="storeName" className="block mb-2">
               Store Name
@@ -98,14 +126,14 @@ export default function VendorRegister() {
               value={formData.storeName}
               onChange={handleChange}
               required
-              className="w-full px-3 py-2 border rounded"
+              className="w-full px-3 py-2 border rounded outline-none focus:ring-1 focus:ring-[#19381f]"
             />
           </div>
           <button
             type="submit"
             className="w-full bg-[#19381f] text-white py-2 rounded hover:bg-[#19381f]/80"
           >
-            Register
+            {loading ? "Loading..." : "Register"}
           </button>
         </form>
         <p className="text-center mt-4">
--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,44 @@
+"use client";
+
+import { useState } from "react";
 import Link from "next/link";
+import { ShoppingCartSimple, List, X } from "@phosphor-icons/react";
+// import { ShoppingBag } from "@phosphor-icons/react";
+import { useCartContext } from "./CartProvider";
 
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { cart } = useCartContext();
+
+  const numOfCartItem = cart.reduce((total, item) => total + item.quantity, 0);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className="bg-white shadow-md">
-      <nav className="container mx-auto px-6 py-3">
+      <nav className="container mx-auto px-6 py-4">
         <div className="flex justify-between items-center">
           <Link href="/" className="text-2xl font-bold text-[#19381f]">
             GreenEats
           </Link>
-          <div className="space-x-4">
+
+          {/* Hamburger menu for mobile */}
+          <div className="lg:hidden flex gap-2 items-center">
+            <Link href="/cart" className="relative">
+              <ShoppingCartSimple size={32} />
+              <div className="absolute -top-2 -right-2 bg-[#19381f] text-white rounded-full w-5 h-5 flex items-center justify-center text-xs">
+                {numOfCartItem}
+              </div>
+            </Link>
+            <button onClick={toggleMenu} className="text-[#19381f]">
+              {isMenuOpen ? <X size={32} /> : <List size={32} />}
+            </button>
+          </div>
+
+          {/* Desktop menu */}
+          <div className="hidden lg:flex gap-5 items-center">
             <Link
               href="/vendor/login"
               className="text-[#19381f] hover:text-[#19381f]/80"
@@ -21,8 +51,34 @@ export default function Header() {
             >
               Order Now
             </Link>
+            <Link href="/cart" className="relative">
+              <ShoppingCartSimple size={32} />
+              <div className="absolute -top-2 -right-2 bg-[#19381f] text-white rounded-full w-5 h-5 flex items-center justify-center text-xs">
+                {numOfCartItem}
+              </div>
+            </Link>
           </div>
         </div>
+
+        {/* Mobile menu */}
+        {isMenuOpen && (
+          <div className="lg:hidden mt-4 flex flex-col gap-4">
+            <Link
+              href="/vendor/login"
+              className="bg-white text-[#19381f] border border-[#19381f] px-4 py-2 rounded hover:bg-[#19381f]/80 text-center"
+              onClick={toggleMenu}
+            >
+              Vendor Login
+            </Link>
+            <Link
+              href="/menu"
+              className="bg-[#19381f] text-white px-4 py-2 rounded hover:bg-[#19381f]/80 text-center"
+              onClick={toggleMenu}
+            >
+              Order Now
+            </Link>
+          </div>
+        )}
       </nav>
     </header>
   );
